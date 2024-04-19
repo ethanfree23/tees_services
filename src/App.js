a37a6061c631ba466053dd24b313f261590b83ea
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-04-19 19:59:01 +0000

    copy finished, just need to add contact section

--- src/App.js
+++ src/App.js
@@ -419,11 +419,11 @@ function App() {
           <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
             <h2 className="text-base font-semibold leading-8 text-indigo-400">Our track record</h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Trusted by dozens of homeowners&nbsp;worldwide
+              <span>Trusted by homeowners&nbsp;</span>
+              <span>in the Greater Conroe area</span>
             </p>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
-              dolor cupiditate blanditiis ratione.
+              Committed to delivering exceptional service to homeowners across the region with unparalleled expertise and meticulous attention to detail.
             </p>
           </div>
           <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-white sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
@@ -481,8 +481,7 @@ function App() {
               Get a quote today.
             </h2>
             <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-              Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
-              commodo do ea.
+              Let's get started with a simple conversation and see if it makes sense to move forward.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
